44656100bd8a006b864381256f638b1c308cc792
larocokarl <geoffreylaroco01@gmail.com>
2026-03-16 18:08:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+function upDate(previewPic) {
+    console.log("upDate triggered for:", previewPic.alt);
+    const imageDiv = document.getElementById('image');
+    imageDiv.innerHTML = previewPic.alt;
+    imageDiv.style.backgroundImage = `url('${previewPic.src}')`;
+}
 
+function unDo() {
+    console.log("unDo triggered");
+    const imageDiv = document.getElementById('image');
+    imageDiv.innerHTML = "Hover over or focus an image below to display here.";
+    imageDiv.style.backgroundImage = "none";
+}
+
+
+function addTabFocus() {
+    console.log("Adding tabindex to all images");
+    const imgs = document.querySelectorAll("img");
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].setAttribute("tabindex", "0");
+    }
+}
+
+window.onload = addTabFocus;
